c90ac67bb82fef1909b66a757748177a80342460
davidcai <w.david.cai@gmail.com>
2018-08-02 22:07:17 -0700

    chore(*): removed unnecessary store import

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "unstated";
 import { TaskListContainer } from "./components/task-list";
-import "./store";
 
 const App = () => (
   <Provider>
